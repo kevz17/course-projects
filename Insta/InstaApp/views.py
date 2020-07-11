9b965ee6cb6853caaee05a3b8754c7202234480b
zwzhang <zhang.zhiwei@northeastern.edu>
2020-07-11 16:26:32 -0700

    add authentication and authorization

--- Insta/InstaApp/views.py
+++ Insta/InstaApp/views.py
@@ -1,7 +1,11 @@
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from InstaApp.models import Post
+from InstaApp.forms import CustomUserCreationForm
 
 class HelloWorld(TemplateView):
     template_name = 'test.html'
@@ -14,10 +18,11 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_create.html'
     fields = '__all__'
+    login_url = 'login'
 
 class PostUpdateView(UpdateView):
     model = Post
@@ -28,3 +33,8 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'signup.html'
+    success_url = reverse_lazy("login")

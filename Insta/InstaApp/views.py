c6aeefe12e62a355d2f1c6c1187cacbd4f94e783
zwzhang <zhang.zhiwei@northeastern.edu>
2020-07-10 20:29:37 -0700

    add CRUD function

--- Insta/InstaApp/views.py
+++ Insta/InstaApp/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView, ListView, DetailView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
 from InstaApp.models import Post
 
 class HelloWorld(TemplateView):
@@ -12,3 +13,18 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_update.html'
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("posts")

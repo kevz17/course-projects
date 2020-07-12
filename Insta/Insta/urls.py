c115bc28d404c00423ca4f1dd933c2ddc498a330
kevz17 <zhiwei.zzw@gmail.com>
2020-07-11 23:51:09 -0700

    complete basic func

--- Insta/Insta/urls.py
+++ Insta/Insta/urls.py
@@ -1,7 +1,6 @@
-"""Insta URL Configuration
-
+"""InstaDemo URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,14 +12,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
 
-from InstaApp.views import SignUp
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('InstaApp.urls')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('auth/signup/', SignUp.as_view(), name='signup'),
+    path('', include('Insta.urls')),
+    path('auth/', include('django.contrib.auth.urls'))
 ]
--- Insta/InstaApp/models.py
+++ Insta/InstaApp/models.py
@@ -1,19 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
 from django.urls import reverse
-
+from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
+
 class InstaUser(AbstractUser):
     profile_pic = ProcessedImageField(
         upload_to='static/images/profiles',
         format='JPEG',
-        options={'quality':100},
-        blank=True,
+        options={'quality': 100},
         null=True,
-    )
+        blank=True,
+        )
 
     def get_connections(self):
         connections = UserConnection.objects.filter(creator=self)
@@ -27,8 +26,8 @@ class InstaUser(AbstractUser):
         followers = UserConnection.objects.filter(following=self)
         return followers.filter(creator=user).exists()
 
-    # def get_absolute_url(self):
-    #     return reverse('profile', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('profile', args=[str(self.id)])
 
     def __str__(self):
         return self.username
@@ -48,41 +47,54 @@ class UserConnection(models.Model):
     def __str__(self):
         return self.creator.username + ' follows ' + self.following.username
 
-
 class Post(models.Model):
-    author = models.ForeignKey(
-        InstaUser,
-        on_delete=models.CASCADE,
-        related_name='my_posts'
-    )
+    author = models.ForeignKey( # a foreign key indicate a Many-To-One relationship
+        InstaUser, #foreign key is InstaUser
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE, # delete this author will delete all his posts
+        related_name='posts', # we can use author.posts to get all posts belong to this user
+        )
     title = models.TextField(blank=True, null=True)
     image = ProcessedImageField(
         upload_to='static/images/posts',
         format='JPEG',
-        options={'quality':100},
+        options={'quality': 100},
         blank=True,
         null=True,
+        )
+    posted_on = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
     )
 
+    def __str__(self):
+        return self.title
+
     def get_absolute_url(self):
-        return reverse("post_detail", args=[str(self.id)])
+        return reverse("post", args=[str(self.id)])
 
     def get_like_count(self):
         return self.likes.count()
 
+    def get_comment_count(self):
+        return self.comments.count()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments',)
+    user = models.ForeignKey(InstaUser, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
+    posted_on = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def __str__(self):
+        return self.comment
 
 class Like(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name='likes')
-    user = models.ForeignKey(
-        InstaUser,
-        on_delete=models.CASCADE,
-        related_name='likes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes',)
+    user = models.ForeignKey(InstaUser, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ("post", "user")
 
     def __str__(self):
-        return 'Like: ' + self.user.username + 'likes ' + self.post.title
+        return 'Like: ' + self.user.username + ' ' + self.post.title

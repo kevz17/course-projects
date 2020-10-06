431c2e4d6b78eb1bc36ecbb8a8e0691e9ef2cbe4
kevz17 <zhiwei.zzw@gmail.com>
2020-10-06 13:47:24 -0700

    Try to fix deployment issue

--- Insta/Insta/settings.py
+++ Insta/Insta/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
